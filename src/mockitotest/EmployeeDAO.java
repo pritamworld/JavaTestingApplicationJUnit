5d7247dd1704e13b7eb82cde20f6ec9b2a3a4e5e
pritesh.patel <pritam.world@gmail.com>
2017-11-08 12:40:30 -0500

    Mockito Test Support added

---
+++ src/mockitotest/EmployeeDAO.java
@@ -0,0 +1,53 @@
+package mockitotest;
+
+import java.util.Collections;
+import java.util.List;
+
+public class EmployeeDAO {
+
+    // creating an object
+    private static EmployeeDAO employeeDAO = new EmployeeDAO();
+
+    /*
+     * private constructor. No objects of this class be created with new
+     * EmployeeDAO(). Use getInstance() instead
+     */
+    private EmployeeDAO() {
+    }
+
+    // static method to create object
+    public static EmployeeDAO getInstance() {
+        return employeeDAO;
+    }
+
+    // add employee
+    public Employee getEmployee(Long employeeId) {
+        return null;
+    }
+
+    // list all employees
+    public List getAll() {
+        return Collections.emptyList();
+    }
+
+    // add employee
+    public String addEmployee(Employee employee) {
+        return employee.getEmployeeName();
+    }
+
+    // update employee
+    public String updateEmployee(Employee employee) {
+        return employee.getEmployeeName();
+    }
+
+    // delete employee
+    public String deleteEmployee(Long employeeId) {
+        return null;
+    }
+
+    // get all awards
+    public int getAwards(Long employeeId) {
+        return 0;
+    }
+}
+
